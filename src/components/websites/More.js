cefb6d1fd735b47db36cbcf07615921550ad9af8
trantien <trantien160398@gmail.com>
2022-04-22 15:35:09 +0700

    update more page

--- src/components/websites/More.js
+++ src/components/websites/More.js
@@ -1,55 +1,164 @@
-import { Avatar, Badge, Col, Divider, Row } from "antd";
+import {
+  QrcodeOutlined,
+  SettingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import {
+  Avatar,
+  Badge,
+  Button,
+  Card,
+  Col,
+  Drawer,
+  message,
+  Popconfirm,
+  Row,
+} from "antd";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../commons/css/avatar-style.css";
 const style = {
-  backgroundColor: "red",
-  height: "18vw",
+  textAlign: "center",
+  marginBottom: "8vw",
 };
+
+const key = "updatable";
+
 const More = () => {
+  let navigate = useNavigate();
+
+  const openMessage = () => {
+    message.loading({ content: "Loading...", key });
+    setTimeout(() => {
+      message.warning({
+        content: "Chức năng đang được xây dựng",
+        key,
+        duration: 2,
+      });
+    }, 2000);
+  };
+
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const backgroundColor = (e) => {
+    setTimeout(() => {
+      localStorage.setItem("backgroundColor", e.target.value);
+    }, 2000);
+  };
+
+  function confirm(e) {
+    console.log(e);
+    message.success("Đã reset về mặc định");
+    localStorage.removeItem("backgroundColor");
+  }
+
   return (
     <div className="more">
       <Row gutter={16}>
-        <Col className="gutter-row" span={6}>
-          <Badge count={99}>
-            <Avatar shape="square" size="large" />
-          </Badge>
+        <Col style={style} className="gutter-row" span={6} onClick={showDrawer}>
+          <Avatar
+            shape="square"
+            className="icon-app-style"
+            icon={<SettingOutlined />}
+          />
         </Col>
-        <Col className="gutter-row" span={6}>
-          <Badge count={99}>
-            <Avatar shape="square" size="large" />
-          </Badge>
+        <Col style={style} className="gutter-row" span={6}>
+          <Avatar
+            shape="square"
+            className="icon-app-style"
+            icon={<QrcodeOutlined />}
+            onClick={openMessage}
+          />
         </Col>
-        <Col className="gutter-row" span={6}>
-          <Badge count={99}>
-            <Avatar shape="square" size="large" />
-          </Badge>
+        <Col style={style} className="gutter-row" span={6}>
+          <Avatar
+            shape="square"
+            className="icon-app-style"
+            icon={<UserOutlined />}
+            onClick={openMessage}
+          />
         </Col>
-        <Col className="gutter-row" span={6}>
-          <Badge count={99}>
-            <Avatar shape="square" size="large" />
-          </Badge>
+        <Col style={style} className="gutter-row" span={6}>
+          <Avatar
+            shape="square"
+            className="icon-app-style"
+            icon={<UserOutlined />}
+            onClick={openMessage}
+          />
+        </Col>
+        <Col style={style} className="gutter-row" span={6}>
+          <Avatar
+            shape="square"
+            className="icon-app-style"
+            icon={<UserOutlined />}
+            onClick={openMessage}
+          />
+        </Col>
+        <Col style={style} className="gutter-row" span={6}>
+          <Avatar
+            shape="square"
+            className="icon-app-style"
+            icon={<UserOutlined />}
+            onClick={openMessage}
+          />
+        </Col>
+        <Col style={style} className="gutter-row" span={6}>
+          <Avatar
+            shape="square"
+            className="icon-app-style"
+            icon={<UserOutlined />}
+            onClick={openMessage}
+          />
+        </Col>
+        <Col style={style} className="gutter-row" span={6}>
+          <Avatar
+            shape="square"
+            className="icon-app-style"
+            icon={<UserOutlined />}
+            onClick={openMessage}
+          />
         </Col>
       </Row>
-      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-        <Col className="gutter-row" span={6}>
-          <Badge count={99}>
-            <Avatar shape="square" size="large" />
-          </Badge>
-        </Col>
-        <Col className="gutter-row" span={6}>
-          <Badge count={99}>
-            <Avatar shape="square" size="large" />
-          </Badge>
-        </Col>
-        <Col className="gutter-row" span={6}>
-          <Badge count={99}>
-            <Avatar shape="square" size="large" />
-          </Badge>
-        </Col>
-        <Col className="gutter-row" span={6}>
-          <Badge count={99}>
-            <Avatar shape="square" size="large" />
-          </Badge>
-        </Col>
-      </Row>
+
+      <Drawer
+        title="Cài đặt"
+        placement="right"
+        onClose={onClose}
+        visible={visible}
+      >
+        <Badge.Ribbon text="Background">
+          <Card title="Cài đặt background" size="small">
+            Vui lòng chọn màu background muốn dùng
+            <p>
+              <input
+                onChange={(e) => backgroundColor(e)}
+                style={{ width: "100%", border: "none" }}
+                type="color"
+              ></input>
+              <Popconfirm
+                title="Bạn có muốn reset về mặc định không?"
+                onConfirm={confirm}
+                onCancel={null}
+                okText="Đồng ý"
+                cancelText="Hủy"
+              >
+                <Button
+                  type="primary"
+                  style={{ width: "100%", border: "none" }}
+                >
+                  Reset
+                </Button>
+              </Popconfirm>
+            </p>
+          </Card>
+        </Badge.Ribbon>
+      </Drawer>
     </div>
   );
 };
