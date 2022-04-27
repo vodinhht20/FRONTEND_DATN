af72fc34ffe7505662b02f123ef45a304d9d6f0b
vodinhht20 <vodinh2000ht@gmail.com>
2022-04-27 23:30:49 +0700

    test api get locaion

--- src/components/websites/Timekeep.js
+++ src/components/websites/Timekeep.js
@@ -1,43 +1,67 @@
-import { InfoCircleOutlined, ShopOutlined, SwapOutlined } from '@ant-design/icons';
-import { Badge, Card, Col, Form, Input, Row, Space, Tooltip, Typography } from 'antd';
+import { InfoCircleOutlined, SwapOutlined } from '@ant-design/icons';
+import { Card, Col, Form, Row, Select, Tooltip} from 'antd';
+import { Option } from 'antd/lib/mentions';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 
 const Timekeep = () => {
 
   const [workspace, setWorkspace] = useState('');
+  const [location, setLocation] = useState({});
+  const [disableSelect, setDisableSelect] = useState(true);
   useEffect(() => {
     // call API
     setWorkspace("Cơ sở 1 Hà Nội");
-  }, [])
+    axios.get('https://api.ipdata.co?api-key=01001158f72f23884238b6b0828bc1edd14985729325a008b06d6670').then(res => {
+      setLocation(res.data.asn)
+    })
+  }, []);
+  const propSelected = {
+    defaultValue: '4',
+    suffix: (
+      <Tooltip title="Cơ sở bạn đang làm việc">
+        <InfoCircleOutlined/>
+      </Tooltip>
+    ),
+    disabled: disableSelect,
+    onSelect: () => {
+      setDisableSelect(true)
+    },
+  }
+
   return (
-      <Space direction="vertical" size="middle" style={{ width: '100%' }}>
-        <Badge.Ribbon text="Cơ sở">
-          <Card size="small" >
-          <Form  labelCol={{ span: 6 }} wrapperCol={{ span: 17 }}>
-            <Form.Item label="Cơ sở làm việc" >
-                <Row align='middle'>
-                  <Col span={18} className="wr-container wrap-item">
-                    <Input prefix={<ShopOutlined />} suffix={
-                        <Tooltip title="Cơ sở bạn đang làm việc">
-                          <InfoCircleOutlined/>
-                        </Tooltip>
-                      }
-                      value={workspace}
-                      disabled={true}
-                    />
-                  </Col>
-                  <Col span={6}>
-                    <Tooltip title="Thay đổi nơi làm việc">
-                      <SwapOutlined className='icon-change-workpace'/>
-                    </Tooltip>
-                  </Col>
-                </Row>
-            </Form.Item>
-          </Form>
-          </Card>
-        </Badge.Ribbon>
-      </Space>
+    <>
+      <Card size="small" >
+        <Form  labelCol={{ span: 6 }} wrapperCol={{ span: 17 }}>
+          <Form.Item label="Cơ sở làm việc" >
+              <Row align='middle'>
+                <Col xs={22} md={20} lg={18}  className="wr-container wrap-item">
+                  <Select {...propSelected}>
+                    <Option value="1">Cơ sở 1 Nam Từ Liêm, HN</Option>
+                    <Option value="2">Cơ sở 2 Ba Đình, HN</Option>
+                    <Option value="3">Cơ sở 3 Gia Lâm, HN</Option>
+                    <Option value="4">Cơ sở 4 Cầu Giấy, HN</Option>
+                  </Select>
+                </Col>
+                <Col xs={2} md={4} lg={6} >
+                  <Tooltip title="Thay đổi nơi làm việc">
+                    <SwapOutlined className='icon-change-workpace' onClick={() => {setDisableSelect(false)}}/>
+                  </Tooltip>
+                </Col>
+              </Row>
+          </Form.Item>
+        </Form>
+      </Card>
+      <Card>
+        <Row justify="center">
+            <Col xs={24} md={20} lg={16}>
+              <p>Địa điểm {location.name}</p>
+              <p>IP Wifi: {location.route}</p>
+            </Col>
+        </Row>
+      </Card>
+    </>
   );
 }
 export default Timekeep;
