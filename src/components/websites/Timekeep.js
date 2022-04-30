8ea9f0bc2603fda4eac980f5f187d5ce74db4980
vodinhht20 <vodinh2000ht@gmail.com>
2022-04-30 18:29:41 +0700

    get work time when checkout

--- src/components/websites/Timekeep.js
+++ src/components/websites/Timekeep.js
@@ -1,75 +1,146 @@
-import { InfoCircleOutlined, SwapOutlined } from '@ant-design/icons';
-import { Card as CardAntd, Col, ConfigProvider, Form, Row, Select, Tooltip} from 'antd';
+import { EnvironmentOutlined, InfoCircleOutlined, SwapOutlined } from '@ant-design/icons';
+import { Card as CardAntd, Col, ConfigProvider, Form, message, Row, Select, Space, Tooltip} from 'antd';
 import { Option } from 'antd/lib/mentions';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
-import { green } from '@mui/material/colors';
+import { orange } from '@mui/material/colors';
 import Card from '@mui/material/Card';
 import Fab from '@mui/material/Fab';
-import CheckIcon from '@mui/icons-material/Check';
-import SaveIcon from '@mui/icons-material/Save';
 import Clock from "react-live-clock";
 import viVN from 'antd/lib/locale/vi_VN';
 import { Typography } from 'antd';
 import FingerprintIcon from '@mui/icons-material/Fingerprint';
+import LogoutIcon from '@mui/icons-material/Logout';
+import moment from "moment";
 
 const Timekeep = () => {
 
-  const [workspace, setWorkspace] = useState('');
+  const [dataTimekeep, setDataTimekeep] = useState({});
   const [location, setLocation] = useState({});
+  const [workSpace, setWorkSpace] = useState();
   const [disableSelect, setDisableSelect] = useState(true);
-  const [loading, setLoading] = React.useState(false);
-  const [success, setSuccess] = React.useState(false);
-  const timer = React.useRef();
+  const [typeCircleLoading, setCircleLoading] = useState(false);
+  const [statusRes, setStatusRes] = useState(true);
   const { Title, Text } = Typography;
-
+  const [loading, setLoading] = useState(false);
+  moment.locale('vn'); 
   useEffect(() => {
-    // call API
-    setWorkspace("Cơ sở 1 Hà Nội");
+
+    // call API location
     axios.get('https://api.ipdata.co?api-key=01001158f72f23884238b6b0828bc1edd14985729325a008b06d6670').then(res => {
       setLocation(res.data.asn)
     });
-    clearTimeout(timer.current);
+
+    // call API timekeep
+    setDataTimekeep({
+      type: 0, // trạng thái checkin 0 chưa checkin, 1 đã checkin
+      fullname: "Võ Văn Định",
+      checkin: null, // thời gian checkin vd: 08:30 default null
+      checkout: null, // thời gian checkout vd: 08:30 default null
+      working_time: null, // tổng thời gian làm 
+      date: "30/04/2022",
+      work_space: '4' // id cơ sở làm việc
+    });
   }, []);
 
   const propSelected = {
-    defaultValue: '4',
+    defaultValue: dataTimekeep.work_space || '1',
     suffix: (
       <Tooltip title="Cơ sở bạn đang làm việc">
         <InfoCircleOutlined/>
       </Tooltip>
     ),
     disabled: disableSelect,
-    onSelect: () => {
-      setDisableSelect(true)
+    onSelect: (e) => {
+      setDisableSelect(true);
+      setCircleLoading(true);
+      // call api
+      setLoading(true);
+      setTimeout(() => {
+        setWorkSpace(e);
+        setLoading(false);
+        setCircleLoading(false);
+        setDataTimekeep({
+          type: 0, // trạng thái checkin 0 chưa checkin, 1 đã checkin
+          fullname: "Võ Văn Định",
+          checkin: null, // thời gian checkin vd: 08:30 default null
+          checkout: null, // thời gian checkout vd: 08:30 default null
+          working_time: null, // tổng thời gian làm 
+          date: "30/04/2022",
+          work_space: '4' // id cơ sở làm việc
+        });
+      }, 2000);
     },
   }
 
-  const handleButtonClick = () => {
-    if (!loading) {
-      setSuccess(false);
-      setLoading(true);
-      timer.current = window.setTimeout(() => {
-        setSuccess(true);
-        setLoading(false);
-      }, 2000);
+  const handleButtonTimekeep = () => {
+    if(statusRes) {
+      setCircleLoading(true);
+      if (dataTimekeep.type) {
+        let checkout = `${moment().format('HH:mm')}`;
+        let startTime = moment(dataTimekeep.checkin, 'HH:mm');
+        let endTime = moment(checkout, 'HH:mm');
+        let workingTime = endTime.diff(startTime, 'hours', true);
+        if(workingTime) {
+          workingTime = workingTime.toFixed(1);
+        } else {
+          workingTime = null;
+        }
+  
+        // call api
+        setStatusRes(false);
+        setTimeout(() => {
+          setCircleLoading(false);
+          setStatusRes(true);
+          setDataTimekeep({
+            ...dataTimekeep,
+            checkout,
+            working_time: workingTime
+          })
+        }, 2000)
+      } else {
+        // call api
+        // setdata
+        setStatusRes(false);
+        setTimeout(() => {
+          setCircleLoading(false);
+          setStatusRes(true);
+          setDataTimekeep({
+            ...dataTimekeep,
+            type: 1,
+            checkin: `${moment().format('HH:mm')}`
+          })
+        }, 2000);
+      }
+    } else {
+      message.warning('Hành động này chúng tôi đang xử lý');
     }
   };
 
   const buttonSx = {
-    ...(success && {
-      bgcolor: green[500],
+    ...(dataTimekeep.type && {
+      bgcolor: orange[500],
       '&:hover': {
-        bgcolor: green[700],
+        bgcolor: orange[700],
       },
     }),
   };
 
+  const circleLoading = {
+    ...(dataTimekeep.type && {
+        color: `${orange[500]} !important`
+    }),
+    position: 'absolute',
+    top: -6,
+    left: -6,
+    zIndex: 1,
+  }
+
   return (
-    <>
+    <div className="time-keep">
       <CardAntd size="small" >
         <Form  labelCol={{ span: 6 }} wrapperCol={{ span: 17 }}>
           <Form.Item label="Cơ sở làm việc" >
@@ -94,7 +165,10 @@ const Timekeep = () => {
       <CardAntd>
         <Row justify="center">
             <Col xs={24} md={20} lg={16} style={{ textAlign: 'center' }}>
-              <Title level={2}>Công ty cổ phần TOPCV</Title>
+              <Title level={3} style={{ marginBlock: '5px' }}>Công ty cổ phần TOPCV</Title>
+              <ConfigProvider locale={viVN}>
+                Chúc bạn một ngày làm việc tốt lành 🎉
+              </ConfigProvider>
               <Title level={3} className="main-clock">
                 <Clock
                   ticking={true}
@@ -102,54 +176,59 @@ const Timekeep = () => {
                   format={"HH:mm:ss"}
                 />
               </Title>
-              <ConfigProvider locale={viVN}>
-                <Clock
-                  ticking={true}
-                  timezone={"Asia/Ho_Chi_Minh"}
-                  format={"MMMM Do YYYY"}
-                />
-              </ConfigProvider>
-              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} className="box-button-time-keep">
                 <Box sx={{ m: 1, position: 'relative' }}>
                   <Fab
                     aria-label="save"
                     color="primary"
                     sx={buttonSx}
-                    onClick={handleButtonClick}
-                    style={{ width: "160px", height: "160px"}}
+                    onClick={handleButtonTimekeep}
+                    style={{ width: "160px", height: "160px", zIndex: 2}}
+                    loading={loading}
                   >
-                    {success ? <CheckIcon /> : <FingerprintIcon fontSize='large'/>}
+                    {
+                      dataTimekeep.type ? 
+                      <Space className="button-content">
+                        <LogoutIcon  className="button-icon"/>
+                        <Text className="lable-button">Check out</Text>
+                      </Space> : 
+                      <Space className="button-content">
+                          <FingerprintIcon fontSize='large' className="button-icon"/>
+                          <Text className="lable-button">Check in</Text>
+                      </Space>
+                    }
                   </Fab>
-                  {loading && (
-                    <CircularProgress
-                      size={173}
-                      sx={{
-                        position: 'absolute',
-                        top: -6,
-                        left: -6,
-                        zIndex: 1,
-                      }}
-                    />
-                    )}
+                  {typeCircleLoading && <CircularProgress size={173} sx={circleLoading} />}
                 </Box>
               </Box>
-              <Title level={5}>Địa điểm làm việc</Title>
-              <Text>{location.name}</Text>
-              <Row>
-                <Col xs={7} md={7} lg={7}>
-                  <Card variant="outlined" sx={{ minWidth: 275 }}>dsdasd</Card>
+              <div className="location-address">
+                <Title level={5}>Địa điểm làm việc</Title>
+                <Text><EnvironmentOutlined /> {location.name}</Text>
+              </div>
+              <Row gutter={[12,12]} className="wrap-time-statistic">
+                <Col span={8}>
+                  <CardAntd variant="outlined" sx={{ width: "100%" }} className="time-item" loading={loading}>
+                    <Title level={3}>{dataTimekeep.checkin ? dataTimekeep.checkin : '--:--'}</Title>
+                    <Text>Giờ vào</Text>
+                  </CardAntd>
                 </Col>
-                <Col xs={7} md={7} lg={7}>
-                  <Card variant="outlined" sx={{ minWidth: 275 }}>dsdasd</Card>
+                <Col span={8}>
+                  <CardAntd variant="outlined" sx={{ width: "100%" }} className="time-item" loading={loading}>
+                    <Title level={3}>{dataTimekeep.checkout ? dataTimekeep.checkout : '--:--'}</Title>
+                    <Text>Giờ ra</Text>
+                  </CardAntd>
                 </Col>
-                <Col xs={7} md={7} lg={7}>
-                  <Card variant="outlined" sx={{ minWidth: 275 }}>dsdasd</Card>
+                <Col span={8}>
+                  <CardAntd variant="outlined" sx={{ width: "100%" }} className="time-item" loading={loading}>
+                    <Title level={3}>{dataTimekeep.working_time ? dataTimekeep.working_time : '0.0'}</Title>
+                    <Text>Số giờ đã làm</Text>
+                  </CardAntd>
                 </Col>
               </Row>
             </Col>
         </Row>
       </CardAntd>
-    </>
+    </div>
   );
 }
 export default Timekeep;
