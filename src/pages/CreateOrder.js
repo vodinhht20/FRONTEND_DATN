996b7d04ded4d2e1a0ff6fdc42071fcb4b1cbc1a
trantien <trantien160398@gmail.com>
2022-05-19 16:48:41 +0700

    Tien-add-send-report-news-list

--- src/pages/CreateOrder.js
+++ src/pages/CreateOrder.js
@@ -1,96 +1,39 @@
-import { MoreOutlined } from "@ant-design/icons";
-import { Alert, Button, Card, Checkbox, Col, Dropdown, PageHeader, Radio, Row } from "antd";
+import { CaretRightFilled } from "@ant-design/icons";
+import { Badge, Card, Col, Row } from "antd";
 import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { initListOrder } from "~/recoil/listOrder";
+import { initLoad } from "~/recoil/loadAtom";
+import { initOrder } from "~/recoil/order";
 
 const CreateOrder = () => {
   const [visible, setVisible] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
+  const loading = useRecoilValue(initLoad);
 
-  const onSubmit = (data) => {
-      console.log(data);
-  };
-  useEffect(() => {
-    document.title = "Đơn nghỉ phép";
-    // call API data
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
-  const DropdownMore = () => (
-    <Dropdown
-      key="more"
-      overlay={<a onClick={() => setVisible(true)}>Báo cáo</a>}
-      placement="bottomRight"
-    >
-      <Button type="text" icon={<MoreOutlined style={{ fontSize: 20 }} />} />
-    </Dropdown>
-  );
+  const listOrder = useRecoilValue(initListOrder);
+  const setOrder = useSetRecoilState(initOrder);
 
   return (
-    <div className="CreateOrder">
-      <PageHeader
-        ghost={false}
-        title="Tạo đơn"
-        extra={[<DropdownMore key="move" />]}
-      />
-
       <Row className="CreateOrder-container" gutter={[12, 12]}>
         <Col xs={24} md={24} lg={24}>
           <Card bordered={false} loading={loading}>
-            <p className="text-info">Ghi rõ lý do vào chú thích nếu có</p>
+            <p className="text-info">Vui lòng chọn mẫu đơn</p>
           </Card>
         </Col>
         <Col xs={24} md={24} lg={24}>
-          <Card title="Trình tạo đơn" bordered={false} loading={loading}>
-            <span className="text-info">
-              <form className="form-create-order" onSubmit={handleSubmit(onSubmit)}>
-                <input placeholder={'Kính gửi *'} className="form-input" {...register("manager_name", {required: true})} />
-                {errors.manager_name && <Alert message="Trường này không được để trống" type="error" showIcon />}
-                <input placeholder={'Tôi tên là: *'} className="form-input" {...register("name", {required: true})} />
-                {errors.name && <Alert message="Trường này không được để trống" type="error" showIcon />}
-
-                <div className="sex">
-                    <span className="text-sex">Giới tính: </span>
-                    <div className="radio-check">
-                        <span>Nam:</span> <input value="nam" type="radio" {...register("sex", {required: true})}/>
-                    </div>
-                    <div className="radio-check">
-                        <span>Nữ:</span> <input value="nu" type="radio" {...register("sex", {required: true})}/>
-                    </div>
-                </div>
-                {errors.sex && <Alert message="Trường này không được để trống" type="error" showIcon />}
-
-                <div className="date">
-                    <span>Thời gian nghỉ:</span>
-                    <div class="date-small">
-                        <span>Từ:</span>
-                        <input placeholder={'Giới tính *'} className="form-input" type="date" {...register("date_time_from", {required: true})} />
-                        {errors.date_time_from && <Alert message="Trường này không được để trống" type="error" showIcon />}
-                    </div>
-                    <div class="date-small">
-                        <span>Đến:</span>
-                        <input placeholder={'Giới tính *'} className="form-input" type="date" {...register("date_time_to", {required: true})} />
-                        {errors.date_time_to && <Alert message="Trường này không được để trống" type="error" showIcon />}
-                    </div>
-                </div>
-                <textarea {...register("lydo", {required: true})} rows="5" className="form-input lydo" placeholder="Lý do"/>
-                <input type="checkbox" className="checkbox" {...register("dongy", {required: true})}/> Tôi xác nhận những điều tôi khai phía trên là đúng sự thật. tôi xin hoàn toàn chịu trách nhiệm với bất cứ sai sót nào
-                {errors.dongy && <Alert message="Vui lòng tích vào ô xác nhận" type="error" showIcon />}
-                <input className="btn" type="submit" />
-              </form>
-            </span>
+          <Card title="Vui lòng chọn loại đơn" bordered={false} loading={loading}>
+            {listOrder && listOrder.map(item => 
+              <Badge.Ribbon text="Đang mở">
+                <Link onClick={() => setOrder(item.name)} key={item.id} to={item.link} className="one-order">
+                  <div className="title-order">{item.name}</div>
+                  <CaretRightFilled className="icon-order" />
+                </Link>
+              </Badge.Ribbon>
+              )}
           </Card>
         </Col>
       </Row>
-    </div>
   );
 };
 export default CreateOrder;
---
+++ src/recoil/listOrder.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const initListOrder = atom({
+    key: "listOrder",
+    default: ""
+});
