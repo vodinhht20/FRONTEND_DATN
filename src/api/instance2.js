6f6ccff58825f200ae0d3b0a342b80b8f916ffc5
Võ Định <71485569+vodinhht20@users.noreply.github.com>
2022-05-25 13:14:23 +0700

    Update instance2.js

--- src/api/instance2.js
+++ src/api/instance2.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const instance2 = axios.create({baseURL: "http://127.0.0.1:8000/api/"});
+const baseURL = process.env.REACT_APP_API;
+const instance2 = axios.create({baseURL});
 
 export default instance2;
