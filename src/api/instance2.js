70695f01829c779e83a6dce2316f5b824feb3e74
trantien <trantien160398@gmail.com>
2022-06-16 10:59:58 +0700

    change server

--- src/api/instance2.js
+++ src/api/instance2.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const baseURL = process.env.REACT_APP_API;
-const baseURL = "http://127.0.0.1:8000/api"
+const baseURL = "http://13.213.62.168/api"
+// const baseURL = "http://127.0.0.1:8000/api"
 const instance2 = axios.create({baseURL});
 
 export default instance2;
