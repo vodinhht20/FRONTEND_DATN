2aab0c02b9d36b516b570940934dc523027b3567
DESKTOP-88FAOV3\EdsoLabs <>
2022-06-20 18:28:46 +0700

    .create

--- src/pages/GiaoDienDonTu/GiaoDienDonTu.js
+++ src/pages/GiaoDienDonTu/GiaoDienDonTu.js
@@ -1,8 +1,191 @@
+import { Button, Form, Input, Select, DatePicker, Space, Table } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+
 const Checkin = () => {
 
+    const [state, setState] = useState('hello world')
+
+    const columns = [
+        {
+            title: 'Full Name',
+            width: 100,
+            dataIndex: 'name',
+            key: 'name',
+            fixed: 'left',
+        },
+        {
+            title: 'Age',
+            width: 100,
+            dataIndex: 'age',
+            key: 'age',
+            fixed: 'left',
+        },
+        {
+            title: 'Column 1',
+            dataIndex: 'address',
+            key: '1',
+            width: 150,
+        },
+        {
+            title: 'Column 2',
+            dataIndex: 'address',
+            key: '2',
+            width: 150,
+        },
+        {
+            title: 'Column 3',
+            dataIndex: 'address',
+            key: '3',
+            width: 150,
+        },
+        {
+            title: 'Column 4',
+            dataIndex: 'address',
+            key: '4',
+            width: 150,
+        },
+        {
+            title: 'Column 5',
+            dataIndex: 'address',
+            key: '5',
+            width: 150,
+        },
+        {
+            title: 'Column 6',
+            dataIndex: 'address',
+            key: '6',
+            width: 150,
+        },
+        {
+            title: 'Column 7',
+            dataIndex: 'address',
+            key: '7',
+            width: 150,
+        },
+        {
+            title: 'Column 8',
+            dataIndex: 'address',
+            key: '8',
+        },
+        {
+            title: 'Action',
+            key: 'operation',
+            fixed: 'right',
+            width: 100,
+            render: () => <a>action</a>,
+        },
+    ];
+    const data = [];
+
+    for (let i = 0; i < 100; i++) {
+        data.push({
+            key: i,
+            name: `Edrward ${i}`,
+            age: 32,
+            address: `London Park no. ${i}`,
+        });
+    }
+
+    const { Option } = Select;
+    const { RangePicker } = DatePicker;
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <div>
-            test
+        <div className="wr-container home-page">
+            <h2>{state}</h2>
+            <Button onClick={() => setState("change State suss")}>changeState</Button>
+
+            <Form
+                name="basic"
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <div>
+                <Form.Item
+                    name="username"
+                > 
+                    <Input placeholder="Nhập nội dung bạn muốn tìm kiếm" suffix={<SearchOutlined />} />
+                </Form.Item>
+                </div>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                >
+                    <Select defaultValue="lucy" style={{ width: 120 }}>
+                        <Option value="jack">Jack</Option>
+                        <Option value="lucy">Lucy</Option>
+                        <Option value="disabled" disabled>
+                            Disabled
+                        </Option>
+                        <Option value="Yiminghe">yiminghe</Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                >
+                    <Select defaultValue="lucy" style={{ width: 120 }}>
+                        <Option value="jack">Jack</Option>
+                        <Option value="lucy">Lucy</Option>
+                        <Option value="disabled" disabled>
+                            Disabled
+                        </Option>
+                        <Option value="Yiminghe">yiminghe</Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                >
+                    <Select defaultValue="lucy" style={{ width: 120 }}>
+                        <Option value="jack">Jack</Option>
+                        <Option value="lucy">Lucy</Option>
+                        <Option value="disabled" disabled>
+                            Disabled
+                        </Option>
+                        <Option value="Yiminghe">yiminghe</Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item>
+                    <Space direction="vertical" size={12}>
+                        <RangePicker />
+
+                    </Space>
+                </Form.Item>
+                <Form.Item>
+                    <Space direction="vertical" size={12}>
+                        <RangePicker />
+
+                    </Space>
+                </Form.Item>
+                <Form.Item
+                >
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+            <span>có 1 đơn từ trong danh sách</span>
+            <Table
+                columns={columns}
+                dataSource={data}
+                scroll={{
+                    x: 1500,
+                    y: 300,
+                }}
+            />
         </div>
     )
 }
