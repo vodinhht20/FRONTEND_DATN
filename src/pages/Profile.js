0c5c022822fd6e78329291a39605817c221187ee
tranhuu.tung <tung.tran@icomm.vn>
2022-07-16 16:20:25 +0700

    cmsn

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,38 @@
-import { CheckOutlined, CloudUploadOutlined, EditOutlined, IdcardOutlined, KeyOutlined } from '@ant-design/icons';
-import { Button, Card, Col, DatePicker, Form, Image, Input, Radio, Row, Select, Spin, Tabs, Typography, notification, Upload } from 'antd';
+import { CheckOutlined, CloudUploadOutlined, EditOutlined, IdcardOutlined, KeyOutlined,CommentOutlined, UserOutlined  } from '@ant-design/icons';
+import { Button, Card, Col, DatePicker, Form, Image, Input, Radio, Row, Select, Spin, Tabs, Typography, notification, Upload,Modal,Avatar,Tooltip ,Popover   } from 'antd';
 import React, { useEffect, useState, useRef } from 'react';
 import { initProfile } from "~/recoil/profile";
 import { useRecoilState } from "recoil";
 import TabChangePassWord from '~/components/Profile/TabChangePassWord';
+import TabMessage from '~/components/Profile/TabMessage'
 import TabProfile from '~/components/Profile/TabProfile';
 const { TabPane } = Tabs;
 
 const Profile = () => {
+// popup danh sách sinh nhật
+const { TextArea } = Input;
+const [isModalVisible, setIsModalVisible] = useState(false);
+const [visible, setVisible] = useState(false);
+const showModal = () => {
+  setIsModalVisible(true);
+};
+
+const handleOk = () => {
+  setIsModalVisible(false);
+};
+
+const handleCancel = () => {
+  setIsModalVisible(false);
+};
+const hide = () => {
+    setVisible(false);
+  };
+
+  const handleVisibleChange = (newVisible) => {
+    setVisible(newVisible);
+  };
+///////////////////
+
     const [profileData, setProfileData] = useRecoilState(initProfile);
     const [loadUpImage, setLoadUpImage] = useState(false);
     const [loadChangePass, setLoadChangePass] = useState(false);
@@ -36,6 +61,28 @@ const Profile = () => {
     console.log(profileData.email);
     return (
         <>
+            <Button type="primary" onClick={showModal}>
+                Danh sách sinh nhật
+            </Button>
+      <Modal title="Danh sách sinh nhật" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <p>
+            <Popover
+                content={ <><TextArea rows={2} />  <a onClick={hide}>Gửi</a></>}
+                title="Gửi lời chúc"
+                placement="rightTop"
+                trigger="click"
+                visible={visible}
+                onVisibleChange={handleVisibleChange}
+            >
+                  <Tooltip placement="top" title={'Tiến Bịp-phòng IT'}> 
+                    <Avatar icon={<UserOutlined />} />
+                  </Tooltip>
+            </Popover>
+
+        </p>
+        <p><Avatar icon={<UserOutlined />} /></p>
+        <p><Avatar icon={<UserOutlined />} /></p>
+      </Modal>
             <Card className="wr-container profile-page">
                 <Tabs tabPosition={ breakpoint < widthScreen ? "left" : "top" }  style={{ width: '100%' }} className="tab-wrap-profile">
                     <TabPane tab={<><IdcardOutlined className="tab-icon"/> <span className="tab-icon-lable">Hồ sơ cá nhân</span></>} key="1">
@@ -43,6 +90,9 @@ const Profile = () => {
                     </TabPane>
                     <TabPane tab={<><KeyOutlined className="tab-icon"/><span className="tab-icon-lable">Thay đổi mật khẩu</span></>} key="2">
                         <TabChangePassWord passwordProps={passwordProps()}/>
+                    </TabPane>
+                    <TabPane tab={<><CommentOutlined className="tab-icon"/><span className="tab-icon-lable">Lời nhắn</span></>} key="3">
+                        <TabMessage />
                     </TabPane>
                     {
                         profileData && 
